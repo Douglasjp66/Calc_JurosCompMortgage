ba28badbc9314a1bf6711bc040e16c2386915d82
Douglasjp66 <83411148+Douglasjp66@users.noreply.github.com>
2021-12-29 09:34:50 -0300

    Project Loan finished

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import  locale
 locale.setlocale(locale.LC_MONETARY,'pt_br')
 
+from mortgage import Loan
+
 from helpers import juros_compostos
+
 if __name__=='__main__':
     quantia_a_emprestar = float(input("Digite um valor a emprestar: "))
     taxa_de_juros_anual = float(input("Digite o valor da taxa de juros anual: "))
     prazo = int(input("Digite o prazo em anos: "))
 
-valor_final_a_pagar= juros_compostos(quantia_a_emprestar,taxa_de_juros_anual,prazo)
+valor_final_a_pagar = juros_compostos(quantia_a_emprestar,taxa_de_juros_anual,prazo)
 print(f"O montante total calculado é: {locale.currency(valor_final_a_pagar,grouping=True)}.")
+print()
+#Usando Mortgege Loan
+financiamento = Loan(quantia_a_emprestar,taxa_de_juros_anual/100,prazo)
+print(financiamento.summarize)
